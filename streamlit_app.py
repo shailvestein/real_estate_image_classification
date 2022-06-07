32601b1ef33d007dc3ef7293278de13a71acdacb
shailvestein <62176093+shailvestein@users.noreply.github.com>
2022-06-07 07:34:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,9 @@ model.load_weights('./efficientNetB0_model.h5')
 image_file = st.file_uploader("Upload Your Image", type=['jpg', 'png', 'jpeg'], accept_multiple_files=True)
 # st.write("filename:", image_file.name)
 
-images = []
+
 if not image_file is None:
+     images = []
      for image in image_file:
           img = Image.open(image)
           img = rf.preprocess_image(img)
