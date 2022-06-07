51f11fc1f369ce1abaf2ba9a83c7791753095aac
shailvestein <62176093+shailvestein@users.noreply.github.com>
2022-06-07 06:29:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,10 +17,10 @@ model.load_weights('./efficientNetB0_model.h5')
 
 
 
-with st.form(my_form):
-    # st.write(Enter your review below)
-    uploaded_files = st.file_uploader("Choose images", accept_multiple_files=True)
-    for uploaded_file in uploaded_files:
-         bytes_data = uploaded_file.read()
-         st.write("filename:", uploaded_file.name)
-         st.write(bytes_data)
+
+# st.write(Enter your review below)
+uploaded_files = st.file_uploader("Choose images", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+     bytes_data = uploaded_file.read()
+     st.write("filename:", uploaded_file.name)
+     st.write(bytes_data)
