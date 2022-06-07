ce663480a42d6cfb4e94d4bb3f69e4f34497820b
shailvestein <62176093+shailvestein@users.noreply.github.com>
2022-06-07 07:33:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ image_file = st.file_uploader("Upload Your Image", type=['jpg', 'png', 'jpeg'], 
 # st.write("filename:", image_file.name)
 
 images = []
-if image_file is not None:
+if not image_file None:
      for image in image_file:
           img = Image.open(image)
           img = rf.preprocess_image(img)
