3101c793094fbd981e69c6d36b17b4f9f67305f1
shailvestein <62176093+shailvestein@users.noreply.github.com>
2022-06-07 06:40:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,8 @@ uploaded_files = st.file_uploader("Choose images", accept_multiple_files=True, t
 for uploaded_file in uploaded_files:
      bytes_data = uploaded_file.read()
      st.write("filename:", uploaded_file.name)
-     st.write(bytes_data)
+     plt.imshow(bytes_data)
+     plt.show()
      
 length = 12
 st.text(f"{length} images uploaded!")
