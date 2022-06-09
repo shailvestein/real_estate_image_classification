e9720e1e3d26c5ad11668b6631df0ac4099eef58
shailvestein <62176093+shailvestein@users.noreply.github.com>
2022-06-09 21:55:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,8 +55,8 @@ def get_model_ready():
     # intialising model
     efficientNet_model = Model(inputs=inputs, outputs=outputs, name='efficientNet_based_model')
     # loading trained weights
-    model = efficientNet_model.load_weights('./efficientNetB0_model.h5')
-    return model
+    efficientNet_model.load_weights('./efficientNetB0_model.h5')
+    return efficientNet_model
 
 
 
